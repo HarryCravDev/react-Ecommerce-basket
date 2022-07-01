931de02b22025717903d3a5c2c30fa20e73a6646
Harry Craven <harrycraven.dev@gmail.com>
2022-07-01 11:55:26 +0100

    feature/store-page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/store-item/StoreItem";
+import Products from "../data/Items.json";
 
 const Store = () => {
-	return <div>Store</div>;
+	return (
+		<>
+			<div>Store</div>
+			<Row xs={1} md={2} lg={3}>
+				{Products.map((product) => (
+					<Col key={product.id} className="mb-3">
+						<StoreItem {...product} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
 };
 
 export default Store;
