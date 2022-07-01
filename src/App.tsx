0d28f63bd334fdade0f4d94ba17139c7df86fb93
Harry Craven <harrycraven.dev@gmail.com>
2022-07-01 11:54:46 +0100

    feature/routes-screen-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Store from "./pages/Store";
+import { ShoppingBasketProvider } from "./context/ShoppingBasketContext";
 
 function App() {
 	return (
 		<div className="App">
-			<Navbar />
-			<Container>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/store" element={<Store />} />
-				</Routes>
-			</Container>
+			<ShoppingBasketProvider>
+				<Navbar />
+				<Container>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/store" element={<Store />} />
+					</Routes>
+				</Container>
+			</ShoppingBasketProvider>
 		</div>
 	);
 }
